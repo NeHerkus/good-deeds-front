0bbc25ef0e35b0097847da8cc940f46042ddcef9
Artur <foox.as23@gmail.com>
2019-03-04 17:32:47 +0200

    Idea service created

--- src/app/idea.service.ts
+++ src/app/idea.service.ts
@@ -6,11 +6,6 @@ import {HttpClient} from '@angular/common/http';
 })
 export class IdeaService {
 
-  getIdea(): Iideas[] {
-    return [
-      {}
-      {}
-    ]
-
+  constructor(private http: HttpClient) {
   }
 }
