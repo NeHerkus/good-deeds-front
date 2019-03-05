88ee38c169a2d13339e0f0087da099c732285298
Herkus Simenas <neherkus@gmail.com>
2019-03-05 17:54:21 +0200

    Edited test for Travis

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
 import {TestBed, async} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
-import {HttpClientModule} from '@angular/common/http';
-import {MatToolbarModule, MatButtonModule, MatIconModule} from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+
 import {HomePageComponent} from './homepage/home-page.component';
 import {NavigationBarComponent} from './navigation-bar/navigation-bar.component';
+import {MatToolbarModule, MatButtonModule, MatIconModule} from '@angular/material';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -15,6 +19,9 @@ describe('AppComponent', () => {
         MatToolbarModule,
         MatButtonModule,
         MatIconModule,
+        AppRoutingModule,
+        BrowserModule,
+        BrowserAnimationsModule
       ],
       declarations: [
         AppComponent,
