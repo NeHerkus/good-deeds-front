50b3be88b92542782588f6dc79ab41fbb56f9af8
Herkus Simenas <neherkus@gmail.com>
2019-03-05 17:15:54 +0200

    Fixed navbar import to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,17 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+
 import {HomePageComponent} from './homepage/home-page.component';
-import {NavigationBarComponent} from './navigation-bar/components/navigation-bar/navigation-bar.component';
 import {MatToolbarModule, MatButtonModule, MatIconModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent,
-    NavigationBarComponent
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
