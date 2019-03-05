046afb1381e8e948d6075e5600335b0c50ed94fa
Herkus Simenas <neherkus@gmail.com>
2019-03-05 16:11:30 +0200

    Fixed homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {HomePageComponent} from './home-page/home-page.component';
+import {HomePageComponent} from './homepage/home-page.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomePageComponent}
+  {path: 'home', component: HomePageComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
-import {HomePageComponent} from './home-page/home-page.component';
+import {HomePageComponent} from './homepage/home-page.component';
 
 @NgModule({
   declarations: [
