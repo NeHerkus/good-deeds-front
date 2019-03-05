2cafcff3d2b4bb5e4ee30aff948ae8de1f1b4c29
ArturasSosnovskis <artur.sosnovskij@me.com>
2019-03-05 16:33:07 +0200

    added Idea interface and new idea form page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NewIdeaFormpageComponent} from './new-idea-formpage/new-idea-formpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'newidea', component: NewIdeaFormpageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AngularMaterialModule} from './angular-material';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NewIdeaFormpageComponent} from './new-idea-formpage/new-idea-formpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewIdeaFormpageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
