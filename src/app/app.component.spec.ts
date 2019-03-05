aa9188e0b53e9f3d49ec936f45c3a2ab10eb6722
Herkus Simenas <neherkus@gmail.com>
2019-03-05 17:37:33 +0200

    Edited installs for Travis

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import {TestBed, async} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
 import {MatToolbarModule, MatButtonModule, MatIconModule} from '@angular/material';
 
 describe('AppComponent', () => {
