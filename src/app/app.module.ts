47bf0eacba73058f2ab7acfe99bb45904e483eca
Herkus Simenas <neherkus@gmail.com>
2019-03-05 17:06:21 +0200

    Added Angular Material Icons and Styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {HomePageComponent} from './homepage/home-page.component';
+import {NavigationBarComponent} from './navigation-bar/components/navigation-bar/navigation-bar.component';
+import {MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navigation-bar/navigation-bar.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {NavigationBarComponent} from './components/navigation-bar/navigation-bar.component';
+
+@NgModule({
+  declarations: [NavigationBarComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class NavigationBarModule {
+}
