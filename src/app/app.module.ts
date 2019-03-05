b8fa9599fe782f90c45bc2a45400265d4bd5fe81
Herkus Simenas <neherkus@gmail.com>
2019-03-05 20:49:09 +0200

    Fix merge
    Somehow the merge conflicts didn't resolve after fixing them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-<<<<<<< HEAD
 import {AngularMaterialModule} from './angular-material';
 import {HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -8,13 +7,6 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {NewIdeaFormpageComponent} from './new-idea-formpage/new-idea-formpage.component';
-=======
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {HttpClientModule} from '@angular/common/http';
-
->>>>>>> feature-toolbar
 import {HomePageComponent} from './homepage/home-page.component';
 import {NavigationBarComponent} from './navigationbar/navigation-bar.component';
 import {MatToolbarModule, MatButtonModule, MatIconModule} from '@angular/material';
@@ -23,27 +15,19 @@ import {MatToolbarModule, MatButtonModule, MatIconModule} from '@angular/materia
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     NewIdeaFormpageComponent,
-    HomePageComponent
-=======
     HomePageComponent,
     NavigationBarComponent
->>>>>>> feature-toolbar
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-<<<<<<< HEAD
     BrowserAnimationsModule,
     AngularMaterialModule
-=======
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule,
-    BrowserAnimationsModule
->>>>>>> feature-toolbar
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
