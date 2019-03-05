1e4d05175de9627cec8030d80fc3f0d85754b133
Artur <foox.as23@gmail.com>
2019-03-05 15:19:55 +0200

    Idea service created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {IdeastableComponent} from './ideastable/ideastable.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Ideastable', component: IdeastableComponent }
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), BrowserModule, FormsModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import { IdeastableComponent } from './ideastable/ideastable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IdeastableComponent
   ],
   imports: [
     BrowserModule,
