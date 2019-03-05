d722b40ac5d7fed5cbf4332805c45d03b853822a
ArturasSosnovskis <artur.sosnovskij@me.com>
2019-03-05 21:09:07 +0200

    Additional new-idea-formpage fixes

--- src/app/angular-material.ts
+++ src/app/angular-material.ts
@@ -4,10 +4,11 @@ import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import {NgModule} from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatInputModule, MatCardModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatInputModule, MatCardModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatInputModule, MatCardModule, MatIconModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatInputModule, MatCardModule, MatIconModule]
 })
 export class AngularMaterialModule {
 }
