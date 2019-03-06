fb1649c548c082f048847ca1fdb5a2945cb6ef72
ArturasSosnovskis <artur.sosnovskij@me.com>
2019-03-06 16:22:07 +0200

    started refactory of new idea formpage component

--- src/app/new-idea-formpage/new-idea-formpage.component.ts
+++ src/app/new-idea-formpage/new-idea-formpage.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {Idea} from '../models/Idea';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-new-idea-formpage',
@@ -7,7 +9,14 @@ import {Component, OnInit} from '@angular/core';
 })
 export class NewIdeaFormpageComponent implements OnInit {
 
-  constructor() {
+  idea: Idea;
+  isReady: false;
+  ideaForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+              private ideadService: IdeaService,
+              private route: ActivatedRoute
+  ) {
 
   }
 
