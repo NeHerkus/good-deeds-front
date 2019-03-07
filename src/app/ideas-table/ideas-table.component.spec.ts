80f0af5b96fdcb15d7789ec27f4d4717f9d668a2
Augustinas <pogis88@gmail.com>
2019-03-07 17:36:48 +0200

    commit before rebase

--- src/app/ideas-table/ideas-table.component.spec.ts
+++ src/app/ideas-table/ideas-table.component.spec.ts
@@ -2,6 +2,7 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {IdeasTableComponent} from './ideas-table.component';
 import {AngularMaterialModule} from '../angular-material';
+import {MatPaginatorModule} from '@angular/material';
 
 
 describe('IdeasTableComponent', () => {
@@ -11,7 +12,7 @@ describe('IdeasTableComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [IdeasTableComponent],
-      imports: [AngularMaterialModule]
+      imports: [AngularMaterialModule, MatPaginatorModule]
     })
       .compileComponents();
   }));
