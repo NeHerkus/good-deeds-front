63fdf6becd68facd5b34d35249eaf2d2639384a0
Augustinas <pogis88@gmail.com>
2019-03-07 14:14:52 +0200

    added table pagination

--- src/app/angular-material.ts
+++ src/app/angular-material.ts
@@ -1,15 +1,15 @@
 // included an additional angular-material.ts file for imports from Angular Material
 
 
-import {MatButtonModule, MatCheckboxModule, MatTableModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatPaginatorModule, MatTableModule} from '@angular/material';
 import {NgModule} from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatInputModule, MatCardModule, MatIconModule, MatTableModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatInputModule, MatCardModule, MatIconModule, MatTableModule]
+  imports: [MatButtonModule, MatCheckboxModule, MatInputModule, MatCardModule, MatIconModule, MatTableModule, MatPaginatorModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatInputModule, MatCardModule, MatIconModule, MatTableModule, MatPaginatorModule]
 })
 export class AngularMaterialModule {
 }
