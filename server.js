685174a90f2f9f3efb5510ed9462585b470185db
Herkus Simenas <neherkus@gmail.com>
2019-03-08 15:05:19 +0200

    sever.js config fix for Heroku routing

--- server.js
+++ server.js
@@ -2,15 +2,18 @@ var express = require('express');
 var compression = require('compression');
 var proxy = require('http-proxy-middleware');
 var API_HOST = process.env.API_HOST || 'localhost:8080';
-var PORT = process.env.PORT || 8080
+var PORT = process.env.PORT || 8080;
+const path = require('path');
 
-var buildPath = 'dist/good-deeds-front'
+var buildPath = 'dist/good-deeds-front';
 
 // Initialize
 var app = express();
 
 // Serve static resources from 'build' folder
 app.use(express.static(buildPath));
+
+app.use(express.static(__dirname + '/dist'));
 
 // Enable gzip response compression
 app.use(compression());
@@ -24,9 +27,8 @@ app.use('/api', proxy({
   }
 }));
 
-// Otherwise serve index.html
-app.get('*', function (req, res) {
-  res.sendFile(__dirname + buildPath + "/index.html");
-});
-
 app.listen(PORT);
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
