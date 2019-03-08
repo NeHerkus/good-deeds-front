e361184c78e25c19eaa30167bcddc5502ef8ff90
Herkus Simenas <neherkus@gmail.com>
2019-03-08 18:07:41 +0200

    Fix User service test

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -1,12 +1,21 @@
 import {TestBed} from '@angular/core/testing';
 
 import {UserService} from './user.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('UserService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: UserService;
+
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [UserService]
+  }));
+
+  beforeEach(() => {
+    service = TestBed.get(UserService);
+  });
 
   it('should be created', () => {
-    const service: UserService = TestBed.get(UserService);
     expect(service).toBeTruthy();
   });
 });
