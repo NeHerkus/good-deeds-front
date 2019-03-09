69cef46fa2916c1c627f0f85e117a6fb0849e768
Artur <foox.as23@gmail.com>
2019-03-09 17:10:01 +0200

    authentication guard and service created

--- src/app/navigationbar/navigation-bar.component.ts
+++ src/app/navigationbar/navigation-bar.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Form, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -6,11 +7,33 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./navigation-bar.component.css']
 })
 export class NavigationBarComponent implements OnInit {
+  form: FormGroup;
+  private formSubmitAttempt: boolean;
 
-  constructor() {
+  constructor(private fb: FormBuilder,
+              private authService: Authservice) {
+
+
   }
 
   ngOnInit() {
+
+    this.form = this.fb.group({
+      email: ['', Validators.required()],
+      password: ['', Validators.required()]
+    });
+  }
+  isFieldInvalid(field: string) {
+    return (
+      (!this.form.get(field).valid && this.form.get(field).touched) ||
+      (this.form.get(field).untouched && this.formSubmitAttempt)
+    );
   }
 
+  onSubmit() {
+    if (this.form.valid) {
+      this.authService.login(this.form.value); // {7}
+    }
+    this.formSubmitAttempt = true;             // {8}
+  }
 }
