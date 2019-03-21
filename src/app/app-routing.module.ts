80c8668321a2db76eaf7f93b37c6af712c33714b
ArturasSosnovskis <artur.sosnovskij@me.com>
2019-03-21 12:01:50 +0200

    table and new idea in the now on different pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import {NewIdeaFormpageComponent} from './newideaformpage/new-idea-formpage.comp
 import {NewUserFormpageComponent} from './newuserformpage/new-user-formpage.component';
 import {PageNotFoundComponent} from './pagenotfound/page-not-found.component';
 import {LoginSignupPageComponent} from './loginsignuppage/login-signup-page.component';
+import {IdeasTableComponent} from './ideastable/ideas-table.component';
 
 const routes: Routes = [
   {path: 'home', component: HomePageComponent},
   {path: 'newidea', component: NewIdeaFormpageComponent},
   {path: 'newuser', component: NewUserFormpageComponent},
   {path: 'login', component: LoginSignupPageComponent},
+  {path: 'deeds', component: IdeasTableComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'notfound', component: PageNotFoundComponent},
   {path: '**', redirectTo: '/notfound', pathMatch: 'full'}
